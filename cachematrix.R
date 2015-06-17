c0c2f162d23ac56e3fee74d52a0ee77356dcb7ff
sineco <sineco@gmail.com>
2015-06-16 20:02:10 -0400

    Added setters and getters

--- cachematrix.R
+++ cachematrix.R
@@ -3,7 +3,15 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    inv <- NULL
+    set <- function(y){
+        x <<- y
+        inv <<- NULL
+    }
+    get <- function() x
+    setinv <- function(inverseMatrix) inv <<- inverseMatrix
+    getinv <- function() inv
+    list(set = set, get = get, setinv = setinv, getinv = getinv)
 }
 
 
